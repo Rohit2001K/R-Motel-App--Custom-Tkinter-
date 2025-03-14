88ef83238f49fe346f82af2c6b27b0a5f5555599
Rohit Kumawat <rohitkumavatgame@gmail.com>
2025-03-14 16:49:50 +0530

    Login UI updated, login function

--- R motel/main.py
+++ R motel/main.py
@@ -22,7 +22,7 @@ class app:
     
     #login window
     def login_form(self):
-        background=canvas = Canvas(self.window,bg ="white",height = 600,width = 900,bd = 0,highlightthickness = 0,relief = "ridge") #intial white background
+        self.background=canvas= Canvas(self.window,bg ="white",height = 600,width = 900,bd = 0,highlightthickness = 0,relief = "ridge") #intial white background
 
         #loading login img and placing
         login_img=canvas.place(x = 0, y = 0)
@@ -41,7 +41,7 @@ class app:
         useremail_text=canvas.create_text(487.0,184.0,anchor="nw",text="Email :",fill="#004B6A",font=("Bungee Regular", 16 * -1))
         useremail_img=PhotoImage(file=self.relative_to_assets("username_entry.png"))
         useremail_img_background= canvas.create_image(635.0,231.0,image=useremail_img)
-        self.email = Entry(bd=0,bg="#D9D9D9",fg="#000716",highlightthickness=0)
+        self.email = Entry(bd=0,bg="#D9D9D9",fg="#000716",highlightthickness=0,font=(10))
         self.email.place(x=486.0,y=217.0,width=292.0,height=30.0)
         useremail_img.image =useremail_img
 
@@ -49,40 +49,66 @@ class app:
         password_text=canvas.create_text(487.0,262.0,anchor="nw",text="Password:",fill="#004B6A",font=("Bungee Regular", 16 * -1))
         password_img = PhotoImage(file=self.relative_to_assets("username_entry.png"))
         password_img_background=canvas.create_image(635.0,308.0,image=password_img)
-        self.password=Entry(bd=0,bg="#D9D9D9",fg="#000716",highlightthickness=0)
+        self.password=Entry(bd=0,bg="#D9D9D9",fg="#000716",highlightthickness=0,show="*",font=(12))
         self.password.place(x=486.0,y=293.0,width=292.0,height=30.0)
         password_img.image=password_img
+
+        #login form message
+        self.login_msg=canvas.create_text(486.0,335.0,anchor="nw",text="Please Enter  Your Email and Password",fill="#004B6A",font=("Bungee Regular", 12 * -1))
 
         #login button
         button_img=button_image_1 = PhotoImage(file=self.relative_to_assets("login_button.png"))
         login_button=Button(image=button_img,borderwidth=0,highlightthickness=0,command=self.LoginUser,relief="flat")
-        login_button.place(x=480.0,y=350.0,width=310.0,height=40.0)
+        login_button.place(x=486.0,y=370.0,width=310.0,height=40.0)
         button_img.image=button_img
         
-        
+        #or
+        or_img = PhotoImage(file=self.relative_to_assets("login_or.png"))
+        login_or_img=canvas.create_image(640.0,457.0,image=or_img)
+        or_img.image=or_img
+
+        #forgot password UI
+        email_img=PhotoImage(file=self.relative_to_assets("login_form_email.png"))
+        login_email_img = canvas.create_image(509.0,500.0,image=email_img)
+        email_img.image=email_img
+        forgot_password=canvas.create_text(530.0,480.0,anchor="nw",text="Forgot Password?",fill="#004B6A",font=("Bungee Regular", 16 * -1))
+        forgot_password_button_img=PhotoImage(file=self.relative_to_assets("forgot_password.png"))
+        forgot_password_button=Button(image=forgot_password_button_img,borderwidth=0,highlightthickness=0,command=lambda: print("reset clicked"),relief="flat")
+        forgot_password_button.place(x=740.0,y=490.0,width=94.0,height=24.0)
+        forgot_password_button_img.image=forgot_password_button_img
+
+        #sign up UP
+        signup_img=PhotoImage(file=self.relative_to_assets("login_form_signup.png"))
+        login_signup_img= canvas.create_image(509.0,550.0,image=signup_img)
+        signup_img.image=signup_img
+        sing_up=canvas.create_text(530.0,533.0,anchor="nw",text="Don’t Have Account?",fill="#004B6A",font=("Bungee Regular", 16 * -1))
+        sing_up_img=PhotoImage(file=self.relative_to_assets("signup.png"))
+        sing_up_button=Button(image=sing_up_img,borderwidth=0,highlightthickness=0,command=lambda: print("signup clicked"),relief="flat")
+        sing_up_button.place(x=740.0,y=543.0,width=94.0,height=24.0)
+        sing_up_img.image=sing_up_img
+
+
+
     #main login function
     def LoginUser(self):
-        self.email=self.email.get()
+        email=self.email.get()
         password=self.password.get()
-        if not self.email or not password:
-            pass #message(Please Fill INFO)
+        if not email or not password:
+            self.background.itemconfig(self.login_msg, text="Please Enter Your Email and Password First!", fill="red")
         else:
-            self.auth_login = User_actions(self.email,password)  
+            self.auth_login = User_actions(email,password)  
             result = self.auth_login.login_user() 
             if result==False:
-                '''
-                self.login_msg.config(text=f'Invalid username or password')
-                self.user_email.delete(0, END) 
-                self.password.delete(0, END)
-                '''
+                self.background.itemconfig(self.login_msg, text="Invalid username or password!", fill="red")
             elif result[0][4]==True: 
                 #self.show_staff_dashboard()
                 pass
             else: 
                 self.user_name = result[0][1]  
-                self.user_email = self.email  
+                self.user_email =email  
                 self.clear_screen()  
                 print("WORKING")
+                
     
     #clear everything function
     def clear_screen(self):
